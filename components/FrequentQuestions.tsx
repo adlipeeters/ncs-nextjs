2fafa4c3fd7aaa3fce8286540fea241138a03af1
adlipeeters <adlipeeters@gmail.com>
2024-05-26 18:18:51 +0300

    commit

--- components/FrequentQuestions.tsx
+++ components/FrequentQuestions.tsx
@@ -42,13 +42,13 @@ const FrequentQuestions = () => {
                             <AccordionTrigger>Is it styled?</AccordionTrigger>
                             <AccordionContent>
                                 Yes. It comes with default styles that matches the other
-                                components&apos; aesthetic.
+                                components aesthetic.
                             </AccordionContent>
                         </AccordionItem>
                         <AccordionItem value="item-3">
                             <AccordionTrigger>Is it animated?</AccordionTrigger>
                             <AccordionContent>
-                                Yes. It's animated by default, but you can disable it if you prefer.
+                                Yes. It is animated by default, but you can disable it if you prefer.
                             </AccordionContent>
                         </AccordionItem>
                     </Accordion>
