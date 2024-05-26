a7c1d866d8d118f1c5a16c59f939d52a3d55ffb4
adlipeeters <adlipeeters@gmail.com>
2024-05-26 18:34:20 +0300

    commit

--- components/ui/infinite-moving-cards.tsx
+++ components/ui/infinite-moving-cards.tsx
@@ -89,7 +89,7 @@ export const InfiniteMovingCards = ({
             >
                 {items.map((item, idx) => (
                     <li
-                        className="w-[350px] max-w-full relative rounded-2xl border border-b- bg-white flex-shrink-0 border-slate-700 px-8 py-6 md:w-[450px]"
+                        className="w-[350px] max-w-full relative rounded-2xl border border-b- bg-white flex-shrink-0 border-gray-300 px-8 py-6 md:w-[450px]"
                         style={{
                             // background:
                                 // "linear-gradient(180deg, var(--slate-800), var(--slate-900)",
