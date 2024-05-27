006e7a328fcede439da88892f243eb3d0c7a65b9
adlipeeters <adlipeeters@gmail.com>
2024-05-27 21:42:46 +0300

    commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "@/components/ThemeProvider";
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute='class' defaultTheme='light' enableSystem>
       <Component {...pageProps} />
+      <SpeedInsights />
     </ThemeProvider>
   )
 }
